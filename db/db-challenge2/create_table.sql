1763f20b08fa178d1524e938e41b3016d9e6f01f
giyana <wktkktkr5@gmail.com>
2021-06-28 20:27:58 +0900

    [fix]カラム名変更

--- db/db-challenge2/create_table.sql
+++ db/db-challenge2/create_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users(
-    user_id int(11) PRIMARY KEY AUTO_INCREMENT,
+    id int(11) PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) NOT NULL,
-    mail varchar(100) NOT NULL,
-    pass varchar(100) NOT NULL,
-    phone_number_work char(13),
-    phone_number_home char(13),
+    email varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    work_phone_number char(13),
+    mobile_phone_number char(13),
     comment varchar(1000),
     created_at datetime NOT NULL,
     modified datetime NOT NULL,
@@ -12,47 +12,47 @@ CREATE TABLE users(
 );
 
 CREATE TABLE chatrooms(
-    chatroom_id int(11) PRIMARY KEY AUTO_INCREMENT,
-    chatroom_name varchar(100) NOT NULL,
-    chatroom_info varchar(1000),
+    id int(11) PRIMARY KEY AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    infomation varchar(1000),
     is_allow_send_file tinyint(1) DEFAULT 0 NOT NULL,
     is_chatroom_directchat tinyint(1) DEFAULT 0 NOT NULL,
     created_at datetime NOT NULL,
     modified_at datetime NOT NULL,
     is_deleted tinyint(1) DEFAULT 0 NOT NULL,
-    post_user_id int(11) NOT NULL REFERENCES users(user_id),
-    modified_user_id int(11) NOT NULL REFERENCES users(user_id)
+    post_user_id int(11) NOT NULL REFERENCES users(id),
+    modified_user_id int(11) NOT NULL REFERENCES users(id)
 );
 
 CREATE TABLE posts(
-    post_id int(11) PRIMARY KEY AUTO_INCREMENT,
+    id int(11) PRIMARY KEY AUTO_INCREMENT,
     message varchar(1000) NOT NULL,
-    file_name varchar(100),
+    attached_file_name varchar(100),
     created_at datetime NOT NULL,
     modified_at datetime NOT NULL,
     is_deleted tinyint(1) DEFAULT 0 NOT NULL,
-    chatroom_id int(11) NOT NULL REFERENCES chatrooms(chatroom_id),
-    post_user_id int(11) NOT NULL REFERENCES users(user_id),
-    modified_user_id int(11) NOT NULL REFERENCES users(user_id)
+    chatroom_id int(11) NOT NULL REFERENCES chatrooms(id),
+    post_user_id int(11) NOT NULL REFERENCES users(id),
+    modified_user_id int(11) NOT NULL REFERENCES users(id)
 );
 
 CREATE TABLE tasks(
-    task_id int(11) PRIMARY KEY AUTO_INCREMENT,
-    task_info varchar(1000) NOT NULL,
+    id int(11) PRIMARY KEY AUTO_INCREMENT,
+    infomation varchar(1000) NOT NULL,
     deadline_at datetime,
     is_completed tinyint(1) DEFAULT 0 NOT NULL,
     created_at datetime NOT NULL,
     modified_at datetime NOT NULL,
     is_deleted tinyint(1) DEFAULT 0 NOT NULL,
-    post_user_id int(11) NOT NULL REFERENCES users(user_id),
-    officer_user_id int(11) NOT NULL REFERENCES users(user_id),
-    modified_user_id int(11) NOT NULL REFERENCES users(user_id),
-    chatroom_id int(11) NOT NULL REFERENCES chatrooms(chatroom_id)
+    post_user_id int(11) NOT NULL REFERENCES users(id),
+    officer_user_id int(11) NOT NULL REFERENCES users(id),
+    modified_user_id int(11) NOT NULL REFERENCES users(id),
+    chatroom_id int(11) NOT NULL REFERENCES chatrooms(id)
 );
 
 CREATE TABLE join_histries(
     id int(11) PRIMARY KEY AUTO_INCREMENT,
     joined_at datetime NOT NULL,
-    chatroom_id int(11) NOT NULL REFERENCES chatrooms(chatroom_id),
-    user_id int(11) NOT NULL REFERENCES users(user_id)
+    chatroom_id int(11) NOT NULL REFERENCES chatrooms(id),
+    user_id int(11) NOT NULL REFERENCES users(id)
 )
