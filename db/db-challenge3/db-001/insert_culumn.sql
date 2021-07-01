da92358fbb9222da28ae1eece728467146efd331
giyana <wktkktkr5@gmail.com>
2021-07-01 17:27:03 +0900

    [fix]sql文リファクタリング

--- db/db-challenge3/db-001/insert_culumn.sql
+++ db/db-challenge3/db-001/insert_culumn.sql
@@ -1,32 +1,13 @@
-INSERT INTO users(name,email,password,work_phone_number,comment,created_at,modified,is_deleted)
-VALUES("admin","admin@labot.inc","admin","00-0000-0000","このスペースの管理者です。","2020/04/20 15:47:52","2020/04/20 15:47:52",0);
-
-INSERT INTO users(name,email,password,mobile_phone_number,created_at,modified,is_deleted)
-VALUES("mysql","mysql@labot.inc","mysql55","080-1234-1253","2020/04/22 16:02:55","2020/04/22 16:02:55",0);
-
-INSERT INTO users(name,email,password,comment,created_at,modified,is_deleted)
-VALUES("sqlserver","sqlserver@labot.inc","apple","オラクルデータベースが好きです。","2020/04/22 17:32:55","2020/04/22 17:32:55",0);
-
-INSERT INTO users(name,email,password,work_phone_number,created_at,modified,is_deleted)
-VALUES("mariadb","mariadb@labot.inc","maria","03-6666-5879","2020/04/22 18:45:21","2020/04/22 18:45:21",0);
-
-INSERT INTO users(name,email,password,created_at,modified,is_deleted)
-VALUES("laravel","laravel@labot.inc","lara","2020/04/22 18:46:01","2020/04/22 18:46:01",0);
-
-INSERT INTO users(name,email,password,work_phone_number,mobile_phone_number,created_at,modified,is_deleted)
-VALUES("cake","cakephp@labot.inc","cake89","03-987-1234","090-5555-7854","2020/04/23 02:51:06","2020/04/25 11:45:06",0);
-
-INSERT INTO users(name,email,password,created_at,modified,is_deleted)
-VALUES("react","react@labot.inc","node","2020/04/23 09:54:32","2020/04/23 09:54:32",0);
-
-INSERT INTO users(name,email,password,mobile_phone_number,created_at,modified,is_deleted)
-VALUES("html5","html@labot.inc","css3","080-5849-5555","2020/04/23 09:55:14","2020/04/23 09:55:14",0);
-
-INSERT INTO users(name,email,password,comment,created_at,modified,is_deleted)
-VALUES("php","php@labot.inc","php7","伝説のphp6","2020/04/24 01:01:06","2020/04/24 01:01:06",0);
-
-INSERT INTO users(name,email,password,work_phone_number,comment,created_at,modified,is_deleted)
-VALUES("js","javascript@labot.inc","react","0120-555-458","Hello world","2020/04/24 05:03:58","2020/04/24 05:03:58",0);
-
-INSERT INTO users(name,email,password,mobile_phone_number,comment,created_at,modified,is_deleted)
-VALUES("amazon","microsoft@gmail.com","google","080-9999-5555","facebook","2020/04/24 06:00:02","2020/04/26 12:13:43",1);
+INSERT INTO users(name,email,password,work_phone_number,mobile_phone_number,comment,created_at,modified,is_deleted)
+VALUES
+("admin","admin@labot.inc","admin","00-0000-0000",null,"このスペースの管理者です。","2020/04/20 15:47:52","2020/04/20 15:47:52",0),
+("mysql","mysql@labot.inc","mysql55",null,"080-1234-1253",null,"2020/04/22 16:02:55","2020/04/22 16:02:55",0),
+("sqlserver","sqlserver@labot.inc","apple",null,null,"オラクルデータベースが好きです。","2020/04/22 17:32:55","2020/04/22 17:32:55",0),
+("mariadb","mariadb@labot.inc","maria","03-6666-5879",null,null,"2020/04/22 18:45:21","2020/04/22 18:45:21",0),
+("laravel","laravel@labot.inc","lara",null,null,null,"2020/04/22 18:46:01","2020/04/22 18:46:01",0),
+("cake","cakephp@labot.inc","cake89","03-987-1234","090-5555-7854",null,"2020/04/23 02:51:06","2020/04/25 11:45:06",0),
+("react","react@labot.inc","node",null,null,null,"2020/04/23 09:54:32","2020/04/23 09:54:32",0),
+("html5","html@labot.inc","css3",null,"080-5849-5555",null,"2020/04/23 09:55:14","2020/04/23 09:55:14",0),
+("php","php@labot.inc","php7",null,null,"伝説のphp6","2020/04/24 01:01:06","2020/04/24 01:01:06",0),
+("js","javascript@labot.inc","react","0120-555-458",null,"Hello world","2020/04/24 05:03:58","2020/04/24 05:03:58",0),
+("amazon","microsoft@gmail.com","google",null,"080-9999-5555","facebook","2020/04/24 06:00:02","2020/04/26 12:13:43",1);
--- db/db-challenge3/db-004/insert_column.sql
+++ db/db-challenge3/db-004/insert_column.sql
@@ -1,50 +1,19 @@
 INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("このチャットの注意事項を添付しました。確認してください。","注意事項.pdf","2020/04/21 13:11:55","2020/04/21 13:11:55",0,1,1,1);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("確認しました",null,"2020/04/22 16:10:11","2020/04/22 16:10:11",0,1,2,2);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("確認しました！",null,"2020/04/22 17:45:29","2020/04/22 17:45:29",0,1,3,3);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("確認しました。",null,"2020/04/22 18:50:11","2020/04/22 18:50:11",0,1,4,4);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("sqlに関する情報を集めるチャットです！よろしくお願いします！","sql.png","2020/04/24 13:41:40","2020/04/24 13:41:40",0,2,2,2);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("よおｒしくおねがいします！",null,"2020/04/24 13:42:03","2020/04/24 13:42:42",1,2,3,3);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("よろしくお願いします！",null,"2020/04/24 13:44:03","2020/04/24 13:44:03",0,2,3,3);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("プログラミング言語に関するチャットです！","プログラミング.gif","2020/04/25 08:06:49","2020/04/25 08:06:49",0,3,10,10);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("よろしくお願いします！",null,"2020/04/25 15:42:22","2020/04/25 15:42:22",0,3,8,8);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("よろしく！",null,"2020/04/25 19:51:10","2020/04/25 19:51:10",0,3,9,9);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("test",null,"2020/04/25 21:15:26","2020/04/25 21:15:26",0,4,5,5);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("見れました！",null,"2020/04/26 00:12:43","2020/04/26 00:12:43",0,4,7,7);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("test",null,"2020/04/26 10:12:58","2020/04/26 10:12:58",0,5,1,1);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("test",null,"2020/04/26 11:21:44","2020/04/26 11:21:44",0,6,1,1);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("miremasita",null,"2020/04/26 14:21:32","2020/04/26 14:22:45",1,6,3,3);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("test",null,"2020/04/26 16:22:50","2020/04/26 16:22:50",0,7,1,1);
-
-INSERT INTO posts(message,attached_file_name,created_at,modified_at,is_deleted,chatroom_id,post_user_id,modified_user_id)
-VALUES("sss",null,"2020/04/26 17:11:23","2020/04/26 18:09:37",1,7,10,10);
+VALUES
+("このチャットの注意事項を添付しました。確認してください。","注意事項.pdf","2020/04/21 13:11:55","2020/04/21 13:11:55",0,1,1,1),
+("確認しました",null,"2020/04/22 16:10:11","2020/04/22 16:10:11",0,1,2,2),
+("確認しました！",null,"2020/04/22 17:45:29","2020/04/22 17:45:29",0,1,3,3),
+("確認しました。",null,"2020/04/22 18:50:11","2020/04/22 18:50:11",0,1,4,4),
+("sqlに関する情報を集めるチャットです！よろしくお願いします！","sql.png","2020/04/24 13:41:40","2020/04/24 13:41:40",0,2,2,2),
+("よおｒしくおねがいします！",null,"2020/04/24 13:42:03","2020/04/24 13:42:42",1,2,3,3),
+("よろしくお願いします！",null,"2020/04/24 13:44:03","2020/04/24 13:44:03",0,2,3,3),
+("プログラミング言語に関するチャットです！","プログラミング.gif","2020/04/25 08:06:49","2020/04/25 08:06:49",0,3,10,10),
+("よろしくお願いします！",null,"2020/04/25 15:42:22","2020/04/25 15:42:22",0,3,8,8),
+("よろしく！",null,"2020/04/25 19:51:10","2020/04/25 19:51:10",0,3,9,9),
+("test",null,"2020/04/25 21:15:26","2020/04/25 21:15:26",0,4,5,5),
+("見れました！",null,"2020/04/26 00:12:43","2020/04/26 00:12:43",0,4,7,7),
+("test",null,"2020/04/26 10:12:58","2020/04/26 10:12:58",0,5,1,1),
+("test",null,"2020/04/26 11:21:44","2020/04/26 11:21:44",0,6,1,1),
+("miremasita",null,"2020/04/26 14:21:32","2020/04/26 14:22:45",1,6,3,3),
+("test",null,"2020/04/26 16:22:50","2020/04/26 16:22:50",0,7,1,1),
+("sss",null,"2020/04/26 17:11:23","2020/04/26 18:09:37",1,7,10,10);
