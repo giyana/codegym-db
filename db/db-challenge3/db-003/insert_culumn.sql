79b8e99fa00a7be810b297da7f1bb7cfaca2b4af
giyana <wktkktkr5@gmail.com>
2021-07-01 15:42:38 +0900

    [add]課題003完成

---
+++ db/db-challenge3/db-003/insert_culumn.sql
@@ -0,0 +1,83 @@
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES ("2020/04/21 11:26:32",1,1);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES ("2020/04/22 16:02:55",1,2);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/22 17:32:55",1,3);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/22 18:45:21",1,4);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/22 18:46:01",1,5);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 02:51:06",1,6);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 09:54:32",1,7);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 09:55:14",1,8);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 01:01:06",1,9);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 05:03:58",1,10);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 06:00:02",1,11);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 15:42:05",2,2);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 18:22:10",2,3);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 20:14:30",2,4);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 11:03:16",3,8);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 15:08:44",3,9);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 18:01:06",3,10);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 18:14:28",4,5);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 08:24:38",4,6);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 10:11:15",4,7);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 14:34:58",5,1);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 14:34:58",5,2);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 14:45:22",6,1);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/23 14:45:22",6,3);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 10:55:21",7,1);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 10:55:21",7,10);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 11:02:20",8,1);
+
+INSERT INTO join_histries(joined_at,chatroom_id,user_id)
+VALUES("2020/04/24 11:02:20",8,11);
