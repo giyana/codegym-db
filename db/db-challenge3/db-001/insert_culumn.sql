4da2f93e438dd48754db9c167db5151372cf3c9d
giyana <wktkktkr5@gmail.com>
2021-07-01 14:35:00 +0900

    [add]デバッグ用

---
+++ db/db-challenge3/db-001/insert_culumn.sql
@@ -0,0 +1,37 @@
+/* 
+INSERT INTO users(name,email,password,work_phone_number,mobile_phone_number,comment,created_at,modified,is_deleted)
+VALUES();
+ */
+
+INSERT INTO users(name,email,password,work_phone_number,comment,created_at,modified,is_deleted)
+VALUES("admin","admin@labot.inc","admin","00-0000-0000","このスペースの管理者です。","2020/04/20 15:47:52","2020/04/20 15:47:52",0);
+
+INSERT INTO users(name,email,password,mobile_phone_number,created_at,modified,is_deleted)
+VALUES("mysql","mysql@labot.inc","mysql55","080-1234-1253","2020/04/22 16:02:55","2020/04/22 16:02:55",0);
+
+INSERT INTO users(name,email,password,comment,created_at,modified,is_deleted)
+VALUES("sqlserver","sqlserver@labot.inc","apple","オラクルデータベースが好きです。","2020/04/22 17:32:55","2020/04/22 17:32:55",0);
+
+INSERT INTO users(name,email,password,work_phone_number,created_at,modified,is_deleted)
+VALUES("mariadb","mariadb@labot.inc","maria","03-6666-5879","2020/04/22 18:45:21","2020/04/22 18:45:21",0);
+
+INSERT INTO users(name,email,password,created_at,modified,is_deleted)
+VALUES("laravel","laravel@labot.inc","lara","2020/04/22 18:46:01","2020/04/22 18:46:01",0);
+
+INSERT INTO users(name,email,password,work_phone_number,mobile_phone_number,created_at,modified,is_deleted)
+VALUES("cake","cakephp@labot.inc","cake89","03-987-1234","090-5555-7854","2020/04/23 02:51:06","2020/04/25 11:45:06",0);
+
+INSERT INTO users(name,email,password,created_at,modified,is_deleted)
+VALUES("react","react@labot.inc","node","2020/04/23 09:54:32","2020/04/23 09:54:32",0);
+
+INSERT INTO users(name,email,password,mobile_phone_number,created_at,modified,is_deleted)
+VALUES("html5","html@labot.inc,css3","080-5849-5555","2020/04/23 09:55:14","2020/04/23 09:55:14",0);
+
+INSERT INTO users(name,email,password,comment,created_at,modified,is_deleted)
+VALUES("php","php@labot.inc","php7","伝説のphp6","2020/04/24 01:01:06","2020/04/24 01:01:06",0);
+
+INSERT INTO users(name,email,password,work_phone_number,comment,created_at,modified,is_deleted)
+VALUES("js","javascript@labot.inc","react","0120-555-458","Hello world","2020/04/24 05:03:58","2020/04/24 05:03:58",0);
+
+INSERT INTO users(name,email,password,mobile_phone_number,comment,created_at,modified,is_deleted)
+VALUES("amazon","microsoft@gmail.com","google","080-9999-5555","facebook","2020/04/24 06:00:02","2020/04/26 12:13:43",1);
