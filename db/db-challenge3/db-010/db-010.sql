b8e24c8a4c7e037624b8e92712df840fe3ef93ce
giyana <wktkktkr5@gmail.com>
2021-07-02 21:40:24 +0900

    改行修正

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -3,13 +3,18 @@
 -- 　・チャットルームidの昇順で表示"
 -- 　・削除されているユーザーの投稿、削除されている投稿は含まないこと
 -- 　・各チャットルームの最新の投稿をしたユーザー名と投稿日時を表示すること
-
-SELECT users.name AS ユーザー名,chatrooms.name AS チャットルーム名,MAX(chatrooms.created_at) AS 投稿日時
-FROM chatrooms
-JOIN users
-ON chatrooms.post_user_id = users.id
-JOIN posts
-ON chatrooms.id = posts.chatroom_id
-WHERE posts.is_deleted = 0 AND users.is_deleted=0
-GROUP BY chatrooms.id
-ORDER BY chatrooms.id ASC;
+SELECT
+    users.name AS ユーザー名,
+    chatrooms.name AS チャットルーム名,
+    MAX(chatrooms.created_at) AS 投稿日時
+FROM
+    chatrooms
+    JOIN users ON chatrooms.post_user_id = users.id
+    JOIN posts ON chatrooms.id = posts.chatroom_id
+WHERE
+    posts.is_deleted = 0
+    AND users.is_deleted = 0
+GROUP BY
+    chatrooms.id
+ORDER BY
+    chatrooms.id ASC;
